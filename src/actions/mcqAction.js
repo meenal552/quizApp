618d891abbc189b9af63a9fc0e2f4896d5103be5
meenal552 <53682388+meenal552@users.noreply.github.com>
2021-05-11 22:34:02 +0530

    Update mcqAction.js

--- src/actions/mcqAction.js
+++ src/actions/mcqAction.js
@@ -2,7 +2,7 @@ import { FETCH_MCQS } from "./types";
 
 export const fetchMcqs = (id) => (dispatch) => {
   console.log("fetching" + id);
-  fetch(`http://localhost:5000/mcq/${id}`)
+  fetch(`https://quiz--site-server.herokuapp.com/mcq/${id}`)
     .then((res) => res.json())
     .then((mcqs) =>
       dispatch({
